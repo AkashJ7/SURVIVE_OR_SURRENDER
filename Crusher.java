267676adffbeff0f67e37fb9129a7aef7c911e02
Arjun Jagdeesh <2arjun11@gmail.com>
2019-04-13 15:56:42 -0400

    methods for movement of smasher

--- Crusher.java
+++ Crusher.java
@@ -3,4 +3,11 @@ public class Crusher extends GameObject {
 		super(x, y, w, h); // w/h have to be decided on based off sprite
 		// We can use my sprite from my game for this (resized)
 	}
+	public void smash() {
+		//move the crusher down, so you would call smasher1.smash() instead
+		//of smasher1.move("down")
+	}
+	public void recover() {
+		//lifting up at slower rate
+	}
 }
