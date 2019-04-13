867f282e57d0f4414f436566f366ea2b77610d14
Arjun Jagdeesh <2arjun11@gmail.com>
2019-04-13 19:25:08 -0400

    added animate method, but fails testing

--- Crusher.java
+++ Crusher.java
@@ -1,13 +1,35 @@
-public class Crusher extends GameObject {
+//import javax.swing.Timer;
+
+public class Crusher {//extends GameObject {// implements ActionListener {
+	//private Timer timer1;
+	private double moveHeight;
+	private double speed;
+	private double xx;
+	private double yy;
+	private int a = 0;
+
 	public Crusher(double x, double y) {
-		super(x, y, w, h); // w/h have to be decided on based off sprite
+		//super(x, y, 10.0, 10.0); // w/h have to be decided on based off sprite
 		// We can use my sprite from my game for this (resized)
+		xx = x;
+		yy = y;
+
+		moveHeight = 600-(yy + 10);
+		//timer1 = new Timer(10, animate);
+		//timer1.start();
 	}
-	public void smash() {
-		//move the crusher down, so you would call smasher1.smash() instead
-		//of smasher1.move("down")
+	public void animate() {
+		while (a < 100) {
+			while (yy > 600) {
+				speed = -0.1;
+			}
+			while (yy < 50) {
+				speed = 0.2;
+			}
+			yy += speed;
+		}
+		a++;
 	}
-	public void recover() {
-		//lifting up at slower rate
-	}
+
+
 }
