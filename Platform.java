e581dbda80048b7ab938ecef2b14158515a0ed64
Akash Jagdeesh <akashjagdeesh7@gmail.com>
2019-04-14 01:02:31 -0400

    Corrected Platform Class

--- Platform.java
+++ Platform.java
@@ -1,13 +1,6 @@
 public class Platform extends GameObject {
 
-	public Platform(double x, double y, double l, String type) {
-		super(x, y);
-		this.length = l;
-		this.type = type; // HORIZONTAL OR VERTICAL (WALL)
+	public Platform(double x, double y, double rows, double columns) {
+		super(x, y, rows * [width_of_tile], columns * [height_of_tile]);
+		// To be filled after we find tile sprite and decide on size of tile (sprite rescaling)
 	}
-	if (type == "wall") {
-		//code to make the length grow sideways
-	} else if (type == "platform") {
-		//code to make the length show down-up
-	}
-}
