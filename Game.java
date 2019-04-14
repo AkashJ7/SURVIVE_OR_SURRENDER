027643430404356d583ae88fa4ca7ddd537eeba9
Akash Jagdeesh <akashjagdeesh7@gmail.com>
2019-04-14 01:02:04 -0400

    Home Screen and Level Testing

--- Game.java
+++ Game.java
@@ -5,14 +5,13 @@ public class Game {
 
 	public static void main(String[] args) {
 		JFrame window = new JFrame("SURVIVE OR SURRENDER!");
-		window.setSize(Scene.DISPLAYWIDTH, Scene.DISPLAYHEIGHT);
-		window.setLocation(1368/2 - Scene.DISPLAYWIDTH/2, 768/2 - Scene.DISPLAYHEIGHT/2);
+		window.setLayout(null);
+		window.setSize(Scene.DISPLAY_WIDTH, Scene.DISPLAY_HEIGHT);
+		window.setLocation(1368/2 - Scene.DISPLAY_WIDTH/2, 768/2 - Scene.DISPLAY_HEIGHT/2);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.setVisible(true);
 
-		while(true) { Scene.updateScene(); }
+		while(true) { Scene.updateScene(window); }
 	}
 }
-
-//final code for game
--- Scene.java
+++ Scene.java
@@ -1,15 +1,44 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class Scene {
 
 	static String scene = "HOME";
-	static int DISPLAY_WIDTH = 800;
-	static int DISPLAY_HEIGHT = 600;
+	static final int DISPLAY_WIDTH = 800;
+	static final int DISPLAY_HEIGHT = 600;
 
-	public static void updateScene() {
+	public static void updateScene(JFrame frame) {
 		if (scene == "HOME") {
 			JButton start = new JButton("START");
+			start.setFocusPainted(false);
+			start.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					scene = "NEXT";
+				}
+			});
 			start.setFont(new Font ("Sans Serif", Font.PLAIN, 20));
-			start.setBounds(DISPLAY_WIDTH/2 - 40, 450, 100, 40);
-			add(start);
+			start.setBounds(DISPLAY_WIDTH/2 - 50, 450, 100, 40);
+			frame.add(start);
+		}
+		else if (scene == "NEXT") {
+			JButton cone = new JButton("CONTINUE");
+			cone.setFocusPainted(false);
+			cone.setFont(new Font("Sans Serif", Font.BOLD, 20));
+			cone.setBounds(DISPLAY_WIDTH/2 - 75, 350, 150, 40);
+			//JLabel text = new JLabel("NEXT", SwingConstants.LEFT);
+			//text.setFont(new Font("Sans Serif", Font.PLAIN, 20));
+			frame.add(cone);
 		}
 	}
 }
+
+
+/* FUNCTIONING COLLIDING CODE
+	ArrayList<GameObject> obstacles = new ArrayList<GameObject>(0);
+	Player a = new Player();
+	GameObject b = new GameObject(100, 50, 50, 100);
+	obstacles.add(b);
+	a.checkForFailure(obstacles);
+	System.out.println(a.alive);
+*/
