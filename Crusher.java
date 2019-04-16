4ed9f67d1384322624a132128bd759754cfe9811
Arjun Jagdeesh <2arjun11@gmail.com>
2019-04-16 16:52:14 -0400

    animate() for crusher and spikes

--- Crusher.java
+++ Crusher.java
@@ -1,9 +1,9 @@
 import javax.swing.Timer;
 import java.awt.event.*;
 
-public class Crusher implements ActionListener {//extends GameObject {
+public class Crusher extends GameObject implements ActionListener {
 	private Timer timer1;
-	//private double moveHeight;
+	private double moveHeight;
 	private double speed;
 	private double xx;
 	private double yy;
@@ -13,26 +13,12 @@ public class Crusher implements ActionListener {//extends GameObject {
 		// We can use my sprite from my game for this (resized)
 		xx = x;
 		yy = y;
-		timer1 = new Timer(1000, new Listener());
-		timer1.start();
+		moveHeight = 600-(yy + 10);
+
+		ImageIcon crusher = new ImageIcon("Sprites/Metal Crusher.jpg");
 	}
-		//moveHeight = 600-(yy + 10);
 
-		private class Listener implements ActionListener {
-			public void actionPerformed(ActionEvent e)
-				while (yy > 600) {
-						speed = -0.1;
-				}
-				while (yy < 50) {
-					speed = 0.2;
-				}
-				yy += speed;
-				System.out.println(""+yy);
-			}
-		}
-
-
-	/*public void animate() {
+	public void animate(Graphics g) {
 		while (yy > 600) {
 				speed = -0.1;
 		}
@@ -40,15 +26,6 @@ public class Crusher implements ActionListener {//extends GameObject {
 			speed = 0.2;
 		}
 		yy += speed;
-	}*/
-
-
-
-	public static void main (String args[]) {
-		Crusher a = new Crusher(10.0, 10.0);
-		System.out.println(""+a.yy);
-		System.out.println(""+a.timer1.isRunning());
-
-
+		g.drawImage(crusher.getImage(), xx, yy, null);
 	}
 }
--- Spikes.java
+++ Spikes.java
@@ -7,8 +7,17 @@ import javax.swing.*;
 
 public class Spikes extends GameObject {
 
-	public Spikes(int x, int y, int w, int h) {
-		super(x, y, w, h);
+	public Spikes(int x, int y, int w) {
+		super(x, y, w);
+		Imageicon spike = new ImageIcon("Sprites/Ground Spikes.jpg");
 	}
+
+
+	public void animate(Graphics g) {
+		for (int i = 0; i < this.w; i++) {
+			g.drawImage(spike.getImage(), this.x+i*this.w, this.y, null);
+		}
+	}
+
 
 }
