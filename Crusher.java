d0377b461995d19e082c69ddb256ca16e81bc2de
Arjun Jagdeesh <2arjun11@gmail.com>
2019-04-16 11:26:45 -0400

    Added the use of timer(), but throws small error

--- Crusher.java
+++ Crusher.java
@@ -1,12 +1,12 @@
-//import javax.swing.Timer;
+import javax.swing.Timer;
+import java.awt.event.*;
 
-public class Crusher {//extends GameObject {// implements ActionListener {
-	//private Timer timer1;
-	private double moveHeight;
+public class Crusher implements ActionListener {//extends GameObject {
+	private Timer timer1;
+	//private double moveHeight;
 	private double speed;
 	private double xx;
 	private double yy;
-	private int a = 0;
 
 	public Crusher(double x, double y) {
 		//super(x, y, 10.0, 10.0); // w/h have to be decided on based off sprite
@@ -14,22 +14,36 @@ public class Crusher {//extends GameObject {// implements ActionListener {
 		xx = x;
 		yy = y;
 
-		moveHeight = 600-(yy + 10);
-		//timer1 = new Timer(10, animate);
-		//timer1.start();
+		//moveHeight = 600-(yy + 10);
+		timer1 = new Timer(1000, new Listener());
+
+		timer1.start();
 	}
-	public void animate() {
-		while (a < 100) {
-			while (yy > 600) {
+	/*public void animate() {
+		while (yy > 600) {
 				speed = -0.1;
+		}
+		while (yy < 50) {
+			speed = 0.2;
+		}
+		yy += speed;
+	}*/
+
+	private class Listener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			@Override
+			while (yy > 600) {
+					speed = -0.1;
 			}
 			while (yy < 50) {
 				speed = 0.2;
 			}
 			yy += speed;
+			System.out.println(""+yy);
 		}
-		a++;
 	}
+	public static void main (String args[]) {
+		Crusher a = new Crusher(10.0, 10.0);
 
-
+	}
 }
