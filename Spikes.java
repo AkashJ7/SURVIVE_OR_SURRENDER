4b14906928f0be64d7c18516372ecec9c63fd8e0
Arjun Jagdeesh <2arjun11@gmail.com>
2019-04-17 19:59:04 -0400

    spikes merge

--- Spikes.java
+++ Spikes.java
@@ -1,8 +1,22 @@
 import java.awt.Rectangle;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
 
-public class Spikes {
 
-	public Spikes(int x, int y) {
-		super(x, y, w, h)
+public class Spikes extends GameObject {
+
+	public Spikes(double x, double y, double w) {
+		super(x, y, w*10.0, 10.0/*DO CONSTRUCTOR*/);
+		Imageicon spike = new ImageIcon("Sprites/Ground Spikes.png");
 	}
+
+	@Override
+	public void animate(Graphics g) {
+		for (int i = 0; i < this.w; i++) {
+			g.drawImage(spike.getImage(), this.x+i*10.0, this.y, null);
+		}
+	}
+
+
 }
