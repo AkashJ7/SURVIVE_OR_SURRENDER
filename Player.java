2ecd8b29628310fbfaff1bda73fcded9ba651f8e
Arjun Jagdeesh <2arjun11@gmail.com>
2019-04-17 13:43:06 -0400

    player update

--- Player.java
+++ Player.java
@@ -1,27 +1,74 @@
-import java.awt.*; // Image, event.KeyEvent
+import java.awt.*; // Image
+import java.awt.event.*; // event.KeyAdapter
+import java.util.*;
+import javax.swing.*;
 
 public class Player extends GameObject {
 
+	int tries = 0;
+	boolean alive = false;
+	double speed = 3;
+	boolean jump = false;
+	double jumping_time = 0;
+	double jump_height;
+	boolean falling = false;
+	static boolean RIGHT, LEFT, UP;
+
 	public Player() {
-		this.tries = 0;
-		this.alive = true;
+		super(100, 500, 20, 50);
+		alive = true;
+		// Draw sprite 15 left and 5 up of rect
 	}
 
-	public void move() {
-		// If statements for keyboard go here
+	public void move () {
+		if (jump_height > 0) { // If touched ground again
+			jump = false; // Not jumping anymore
+			jumping_time = 0; // Reset parabola
+		}
+		if (UP && falling == false) {
+			jump = true;
+		}
+		if (jump) {
+			jumping_time += 2;
+			jump_height = -(-0.03*jumping_time*jumping_time + 3.5*jumping_time);
+			// jump math prob needs tweaking based on our sizes/game
+		}
+		if (RIGHT) {
+			this.x += speed;
+			//g.draw(this);
+			//System.out.println(this.x);
+			// Sprite animation
+		} else if (LEFT) {
+			this.x -= speed;
+			// Moving left, with opposite sprite animation
+		}
 	}
 
-	public boolean constrain() {
+	public void constrain() {
 		//
 	}
 
-	public void displayTries() {
-		// Stuff from Graphics?
+	public void displayTries(Graphics g) {
+		//
 	}
 
-	public boolean checkForFailure(ArrayList<GameObject> list) {
-		for(danger : list) {
-			if danger.intersects(this) { this.alive = false; }
+	public void checkForFailure(ArrayList<GameObject> obstacles) {
+		for(GameObject danger : obstacles) {
+			if (danger.intersects(this)) { alive = false; }
+		}
+	}
+
+	static class KeyInput extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) RIGHT = true;
+			else if (e.getKeyCode() == KeyEvent.VK_LEFT) LEFT = true;
+			if (e.getKeyCode() == KeyEvent.VK_UP) UP = true;
+		}
+
+		public void keyReleased(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) RIGHT = false;
+			else if (e.getKeyCode() == KeyEvent.VK_LEFT) LEFT = false;
+			if (e.getKeyCode() == KeyEvent.VK_UP) UP = false;
 		}
 	}
 }
